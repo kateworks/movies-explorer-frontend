c84376b0a3c465c3fb34aaa705c0c016380f2f56
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2021-03-12 14:03:45 +0300

    feat: реализовать чтение карточек с сервера и отображение

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import RoundCheckBox from '../RoundCheckBox/RoundCheckBox';
 import Button from '../Button/Button';
 
-import { initialMovies } from '../../utils/movies-init';
+//import { initialMovies } from '../../utils/movies-init';
+import moviesApi from '../../utils/MoviesApi';
+import { MOVIES_URL } from '../../utils/MoviesApi';
 import './Movies.css';
 
 function Movies() {
-  const [moviesList, setMoviesList] = useState(initialMovies);
+  const [moviesList, setMoviesList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const readMovies = () => {
+    setIsLoading(true);
+    moviesApi.getAllMovies()
+      .then((movies) => {
+        console.log(movies);
+        setMoviesList(movies.map(movie => {
+          return {
+            nameRU: movie.nameRU,
+            thumbnail: movie.image ? `${MOVIES_URL}${movie.image.url}` : '',
+            trailerLink: movie.trailerLink,
+            duration: movie.duration,
+            id: movie.id,
+          };
+        }));
+      })
+      .catch((err) => {
+        console.log(`Ошибка загрузки данных: ${err}.`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    readMovies();
+  }, []);
 
   return (
     <section className="movies">
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './MoviesCard.css';
 
-const MoviesCard = ({ movie, children }) => (
-  <li className="card gradual-change">
-      <img
-        className="card__image"
-        src={movie.thumbnail}
-        alt={movie.nameRU}
-      />
+function MoviesCard({ movie, children }) {
+
+  const getMovieDuration = (duration) => {
+    const hours = Math.floor(duration / 60)
+    const minutes = duration - hours * 60;
+    return `${hours}ч${minutes}м`;
+  };
+
+  return (
+    <li className="card gradual-change">
+      <div className="card__wrapper">
+        <img
+          className="card__image"
+          src={movie.thumbnail}
+          alt={movie.nameRU} />
+      </div>
 
       <ul className="card__info">
         <li className="card__name">{movie.nameRU}</li>
         <li>{children}</li>
-        <li className="card__duration">{movie.duration}</li>
+        <li className="card__duration">{getMovieDuration(movie.duration)}</li>
       </ul>
-  </li>
-);
+    </li>
+  );
+}
 
 export default MoviesCard;
